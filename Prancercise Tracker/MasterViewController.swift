4d5e1f60e6d48362388be88fe89484eef28b8a6d
Theodore Bendixson <tedbendixson@me.com>
2017-05-19 15:18:38 -0400

    Removed alerts from HealthKit Authorization

--- Prancercise Tracker/MasterViewController.swift
+++ Prancercise Tracker/MasterViewController.swift
@@ -47,41 +47,4 @@ class MasterViewController: UITableViewController {
       authorizeHealthKit()
     }
   }
-  
-  private func displayAuthorizedAlert() {
-    
-    let alert = UIAlertController(title: nil,
-                                  message: "HealthKit Authorized. Get your Prancercise on!",
-                                  preferredStyle: .alert)
-    
-    alert.addAction(UIAlertAction(title: "O.K.",
-                                  style: .default,
-                                  handler: nil))
-    
-    present(alert, animated: true, completion: nil)
-  }
-  
-  private func displayAuthorizationErrorAlert(with error: Error?) {
-    
-    let baseText = "HealthKit authorized failed"
-    
-    var message = String()
-    
-    if let error = error {
-      message = "\(baseText). Reason: \(error.localizedDescription)"
-    } else {
-      message = baseText
-    }
-    
-    let alert = UIAlertController(title: nil,
-                                  message: message,
-                                  preferredStyle: .alert)
-    
-    alert.addAction(UIAlertAction(title: "O.K.",
-                                  style: .default,
-                                  handler: nil))
-    
-    present(alert, animated: true, completion: nil)
-  }
-  
 }
