fe8aa3c2faa56b5c873ad3d47391c465aec5a174
Theodore Bendixson <tedbendixson@me.com>
2017-05-30 10:51:26 -0400

    Template Code For Part 2 Tutorial

--- Prancercise Tracker/WorkoutDataStore.swift
+++ Prancercise Tracker/WorkoutDataStore.swift
@@ -32,4 +32,9 @@ import HealthKit
 
 class WorkoutDataStore {
   
+  class func save(prancerciseWorkout: PrancerciseWorkout,
+                  completion: @escaping ((Bool, Error?) -> Swift.Void)) {
+    
+  }
+  
 }
